7e1353da494cd6c1c14738a7a647f6ef65436f38
RuoSgeoinfo <46516130+RuoSgeoinfo@users.noreply.github.com>
2019-01-09 14:32:46 +0100

    added variables

--- client/baseWebpack/app/application/base.js
+++ client/baseWebpack/app/application/base.js
@@ -5,6 +5,14 @@ import template from "./base.html";
 class BaseController {
     constructor() {
         this.test ="basecomponent";
+        this.numbers = [1,1,1,1,1,,1,1];
+        this.name = "name";
+        this.button = "click me";
+    }
+
+    clickButton(user){
+        console.log(user);
+        alert(`Hello ${user}`)
     }
 }
 angular.module("baseWebpack").component("baseComponent", {
